b7be875b2abe841c83d2a9b660465371ba62b09b
ducvovan <cloudicvo@vivaldi.net>
2019-11-13 12:18:22 +0700

    change num

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from page.TuoiTre import TuoiTre
 if __name__ == "__main__":
     f = open("./data/tuoitre/tuoitre.json", "a")
     f.write("[")
-    number = 10 # 10*15 page
+    number = 70 # 10*15 page
     tuoitre = TuoiTre(number, f)
     tuoitre.scrapting()
     f.write("{}]")

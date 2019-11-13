4bb33f2d72382d9a3590a51a9ebe5bbf94196440
ducvovan <cloudicvo@vivaldi.net>
2019-11-13 12:16:44 +0700

    final

--- api/TuoiTreAPI.py
+++ api/TuoiTreAPI.py
@@ -25,8 +25,69 @@ class TuoiTreAPI:
                 "mua-sam": 200006,
                 "dau-tu": 200007
             },
-            "cong-nghe": {
+            "cong-nghe":{
                 "nhip-song-so": 16,
+                "trai-nghiem": 557,
+                "thi-truong": 547,
+                "thu-thuat": 67,
+                "gia-dinh-so": 200028,
+                "nhip-cau": 297
+            },
+            "xe":{
+                "xe": 659
+            },
+            "du-lich":{
+                "co-hoi-du-lich": 100001,
+                "di-choi": 100009,
+                "an-gi": 100010,
+                "mach-ban": 384,
+                "que-huong": 100011
+            },
+            "nhip-song-tre": {
+                "xu-huong": 200012,
+                "theo-guong-bac": 1580,
+                "kham-pha": 200013,
+                "yeu": 194,
+                "nhan-vat": 200014
+            },
+            "van-hoa":{
+                "nhan-vat": 200050,
+                "san-dien": 200051,
+                "sach": 200051,
+                "doi-song": 200018
+            },
+            "giai-tri":{
+                "nghe-gi-hom-nay": 1581,
+                "am-nhac": 58,
+                "dien-anh": 57,
+                "tv-show": 385,
+                "thoi-trang": 919,
+                "hau-truong": 922
+            },
+            "the-thao": {
+                "tin-tuc": 200030,
+                "cong-dong": 200031,
+                "binh-luan": 200032,
+                "the-thao-vui": 200034
+            },
+            "giao-duc":{
+                "tuyen-sinh": 142,
+                "hoc-duong": 1507,
+                "bai-giai-dap-an": 200054,
+                "du-hoc": 85,
+                "cau-chuyen-giao-duc": 913,
+                "goc-hoc-tap": 200020
+            },
+            "khoa-hoc": {
+                "thuong-thuc": 200010,
+                "phat-minh": 200011
+            },
+            "suc-khoe":{
+                "dinh-duong": 200008,
+                "me-va-be": 197,
+                "gioi-tinh": 241,
+                "phong-mach": 231,
+                "biet-de-khoe": 1465
             }
         }
 
